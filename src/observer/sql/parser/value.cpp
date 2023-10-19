9dfaf5146392551448ba42ac9b4510df9fe31599
nkufree <2995507298@qq.com>
2023-10-19 17:52:31 +0000

    7.join-tables

--- src/observer/sql/parser/value.cpp
+++ src/observer/sql/parser/value.cpp
@@ -232,18 +232,18 @@ int Value::compare(const Value &other) const
     float other_data = other.num_value_.int_value_;
     return common::compare_float((void *)&this->num_value_.float_value_, (void *)&other_data);
   }else if (this->attr_type_ == CHARS && (other.attr_type_ == FLOATS)) {
-    float this_data = this->str_value_[0];
+    float this_data = atof(this->str_value_.substr(0,this->str_value_.size()-1).c_str());
     return common::compare_float((void *)&this_data, (void *)&other.num_value_.float_value_);
   } else if (this->attr_type_ == FLOATS && other.attr_type_ == CHARS) {
-    float other_data = other.str_value_[0];
+    float other_data = atof(other.str_value_.substr(0,other.str_value_.size()-1).c_str());
     return common::compare_float((void *)&this->num_value_.float_value_, (void *)&other_data);
   }else if (this->attr_type_ == CHARS && (other.attr_type_ == INTS)) {
-    float this_data = this->str_value_[0];
+    float this_data = atof(this->str_value_.substr(0,this->str_value_.size()-1).c_str());
     float other_data = other.num_value_.int_value_;
     return common::compare_float((void *)&this_data, (void *)&other_data);
   } else if (this->attr_type_ == INTS && other.attr_type_ == CHARS) {
     float this_data = this->num_value_.int_value_;
-    float other_data = other.str_value_[0];
+    float other_data = atof(other.str_value_.substr(0,other.str_value_.size()-1).c_str());
     return common::compare_float((void *)&this_data, (void *)&other_data);
   }
   LOG_WARN("not supported");
